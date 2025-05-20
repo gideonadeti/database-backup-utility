7061fe10557f6f046f5610e61d02ea1f63e1c74b
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-20 19:58:46 +0000

    refactor(backup): Remove unused database backup functions

--- src/commands/backup.ts
+++ src/commands/backup.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import {Command, Flags} from '@oclif/core'
 
-import {backupMongo, backupMySQL, backupPostgres, backupSQLite} from '../utils/backup-db.js'
+import {backupPostgres} from '../utils/backup-db.js'
 import detectDbType from '../utils/detect-db-type.js'
 
 export default class Backup extends Command {
@@ -23,24 +23,8 @@ export default class Backup extends Command {
     const dbType = detectDbType(url)
 
     switch (dbType) {
-      case 'mongodb': {
-        return backupMongo(url)
-      }
-
-      case 'mysql': {
-        return backupMySQL(url)
-      }
-
       case 'postgres': {
         return backupPostgres(url)
-      }
-
-      case 'sqlite': {
-        return backupSQLite(url)
-      }
-
-      default: {
-        this.error('Unsupported database type.')
       }
     }
   }
