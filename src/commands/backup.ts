563e3687a3c704a24b300bedfcaa8714bbc63ee2
Gideon Adeti <gideonadeti0@gmail.com>
2025-05-20 15:38:39 +0000

    fix(backup): Ensure database URL is required for backup command
    Updated the backup command to check for a valid database URL.
    If the URL is not provided, an error message will be displayed,
    ensuring that the command cannot run without the necessary
    database connection information.

--- src/commands/backup.ts
+++ src/commands/backup.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import {Command, Flags} from '@oclif/core'
 
 import {backupMongo, backupMySQL, backupPostgres, backupSQLite} from '../utils/backup-db.js'
@@ -7,12 +8,17 @@ export default class Backup extends Command {
   static override description = 'Backup a database'
   static override examples = ['<%= config.bin %> <%= command.id %>']
   static override flags = {
-    url: Flags.string({char: 'u', description: 'Database URL', required: true}),
+    url: Flags.string({char: 'u', description: 'Database URL'}),
   }
 
   public async run() {
     const {flags} = await this.parse(Backup)
-    const {url} = flags
+    const url = flags.url || process.env.DB_URL
+
+    if (!url) {
+      this.error('Database URL is required.')
+    }
+
     const dbType = detectDbType(url)
 
     switch (dbType) {
